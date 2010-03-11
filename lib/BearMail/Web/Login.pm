5dcd191644c874bd542375c97dd123d09925f4d0
zerodeux <zerodeux@c98814c1-5233-0410-b978-935bc6c74651>
2010-03-11 13:35:27 +0000

    Refactored login logic + using simple and single 'master_password' a la Mailman
    git-svn-id: svn+ssh://forge.bearstech.com/var/lib/svn/forge/bearmail@358 c98814c1-5233-0410-b978-935bc6c74651

--- lib/BearMail/Web/Login.pm
+++ lib/BearMail/Web/Login.pm
@@ -28,27 +28,51 @@ sub login : StartRunMode {
     my $email = $q->param('email') || '';
     my $pass  = $q->param('password') || '';
 
-    if(($self->cfg('global_postmaster_login') eq $email)
-	and ($self->cfg('global_postmaster_password') eq md5_hex($pass))) {
+    # We can't proceed with login if we don't have both params
+    return $self->login_page() if $email eq '' or $pass eq '';
 
-        $self->session->param('user', $email);
-        $self->session->param('level', 'admin');
-        my $intent = $self->session->param('intent') || 'domain_list';
-        warn "Login successful, redirecting to intent='$intent'";
-        return $self->redirect($self->url($intent));
+    # FIXME: need to handle simple user login too
 
-    } elsif(exists($self->{b}->get_postmasters()->{$email})
-            and $self->{b}->get_postmasters()->{$email} eq md5_hex($pass)) {
+    # First check simple domain login
+    my $domain_pass = $self->{b}->get_postmasters()->{$email};
+    return $self->login_ok($email, 'postmaster')
+        if defined $domain_pass and $domain_pass eq md5_hex($pass);
 
-        $self->session->param('user', $email);
-        $self->session->param('level', 'postmaster');
-        my $intent = $self->session->param('intent') || 'domain_list';
-        warn "Login successful, redirecting to intent='$intent'";
-        return $self->redirect($self->url($intent));
-    }
+    # Then try master password, but only on amdin|root logins to prevent
+    # users discovering domain/master password collisiona by accident
+    my $master_pass = $self->cfg('master_password');
+    return $self->login_ok($email, 'admin')
+        if defined $master_pass and $master_pass eq md5_hex($pass) and
+           $email =~ /^(admin(inistrator)?|root)$/i;
+
+    return $self->login_page();
+}
+
+sub login_page {
+    my $self = shift;
 
     my $tmpl = $self->load_tmpl('login.html');
     return $tmpl->output;
+}
+
+sub login_ok {
+    my $self = shift;
+    my ($user, $level) = @_;
+
+    # Store authentified user in session (privileges should be checked at
+    # every operation instead of being stored in s{level}, FIXME)
+    $self->session->param('user', $user);
+    $self->session->param('level', $level);
+
+    # Redirect to the original page the user intended to go, or some fitting
+    # default page depending on user privileges.
+    my %default = (
+      user       => 'address_edit',
+      postmaster => 'address_list',
+      admin      => 'domain_list',
+    );
+    my $intent = $self->session->param('intent') || $default{$level};
+    return $self->redirect($self->url($intent));
 }
 
 1;
